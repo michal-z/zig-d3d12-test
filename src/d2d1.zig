9b9b4c8a45c786e11bfff442a404c41b066e23c3
Michal Ziulek <michalziulek@gmail.com>
2020-10-04 15:52:07 +0200

    -

--- src/d2d1.zig
+++ src/d2d1.zig
@@ -592,7 +592,7 @@ pub const IDrawingStateBlock = extern struct {
     usingnamespace IResource.Methods(Self);
 };
 
-pub const IRenderTarget = extern struct {
+pub const IDeviceContext6 = extern struct {
     const Self = @This();
     vtbl: *const extern struct {
         // IUnknown
@@ -655,12 +655,82 @@ pub const IRenderTarget = extern struct {
         GetPixelSize: *c_void,
         GetMaximumBitmapSize: *c_void,
         IsSupported: *c_void,
+        // ID2D1DeviceContext
+        CreateBitmap1: *c_void,
+        CreateBitmapFromWicBitmap1: *c_void,
+        CreateColorContext: *c_void,
+        CreateColorContextFromFilename: *c_void,
+        CreateColorContextFromWicColorContext: *c_void,
+        CreateBitmapFromDxgiSurface: *c_void,
+        CreateEffect: *c_void,
+        CreateGradientStopCollection1: *c_void,
+        CreateImageBrush: *c_void,
+        CreateBitmapBrush1: *c_void,
+        CreateCommandList: *c_void,
+        IsDxgiFormatSupported: *c_void,
+        IsBufferPrecisionSupported: *c_void,
+        GetImageLocalBounds: *c_void,
+        GetImageWorldBounds: *c_void,
+        GetGlyphRunWorldBounds: *c_void,
+        GetDevice: *c_void,
+        SetTarget: *c_void,
+        GetTarget: *c_void,
+        SetRenderingControls: *c_void,
+        GetRenderingControls: *c_void,
+        SetPrimitiveBlend: *c_void,
+        GetPrimitiveBlend: *c_void,
+        SetUnitMode: *c_void,
+        GetUnitMode: *c_void,
+        DrawGlyphRun1: *c_void,
+        DrawImage: *c_void,
+        DrawGdiMetafile: *c_void,
+        DrawBitmap1: *c_void,
+        PushLayer1: *c_void,
+        InvalidateEffectInputRectangle: *c_void,
+        GetEffectInvalidRectangleCount: *c_void,
+        GetEffectInvalidRectangles: *c_void,
+        GetEffectRequiredInputRectangles: *c_void,
+        FillOpacityMask1: *c_void,
+        // ID2D1DeviceContext1
+        CreateFilledGeometryRealization: *c_void,
+        CreateStrokedGeometryRealization: *c_void,
+        DrawGeometryRealization: *c_void,
+        // ID2D1DeviceContext2
+        CreateInk: *c_void,
+        CreateInkStyle: *c_void,
+        CreateGradientMesh: *c_void,
+        CreateImageSourceFromWic: *c_void,
+        CreateLookupTable3D: *c_void,
+        CreateImageSourceFromDxgi: *c_void,
+        GetGradientMeshWorldBounds: *c_void,
+        DrawInk: *c_void,
+        DrawGradientMesh: *c_void,
+        DrawGdiMetafile1: *c_void,
+        CreateTransformedImageSource: *c_void,
+        // ID2D1DeviceContext3
+        CreateSpriteBatch: *c_void,
+        DrawSpriteBatch: *c_void,
+        // ID2D1DeviceContext4
+        CreateSvgGlyphStyle: *c_void,
+        DrawText1: *c_void,
+        DrawTextLayout1: *c_void,
+        DrawColorBitmapGlyphRun: *c_void,
+        DrawSvgGlyphRun: *c_void,
+        GetColorBitmapGlyphImage: *c_void,
+        GetSvgGlyphImage: *c_void,
+        // ID2D1DeviceContext5
+        CreateSvgDocument: *c_void,
+        DrawSvgDocument: *c_void,
+        CreateColorContextFromDxgiColorSpace: *c_void,
+        CreateColorContextFromSimpleColorProfile: *c_void,
+        // ID2D1DeviceContext6
+        BlendImage: *c_void,
     },
     usingnamespace os.IUnknown.Methods(Self);
     usingnamespace IResource.Methods(Self);
 };
 
-pub const IBitmapRenderTarget = extern struct {
+pub const IDevice6 = extern struct {
     const Self = @This();
     vtbl: *const extern struct {
         // IUnknown
@@ -669,224 +739,36 @@ pub const IBitmapRenderTarget = extern struct {
         Release: fn (*Self) callconv(.Stdcall) u32,
         // ID2D1Resource
         GetFactory: fn (*Self, **IFactory) callconv(.Stdcall) void,
-        // ID2D1RenderTarget
-        CreateBitmap: *c_void,
-        CreateBitmapFromWicBitmap: *c_void,
-        CreateSharedBitmap: *c_void,
-        CreateBitmapBrush: *c_void,
-        CreateSolidColorBrush: *c_void,
-        CreateGradientStopCollection: *c_void,
-        CreateLinearGradientBrush: *c_void,
-        CreateRadialGradientBrush: *c_void,
-        CreateCompatibleRenderTarget: *c_void,
-        CreateLayer: *c_void,
-        CreateMesh: *c_void,
-        DrawLine: *c_void,
-        DrawRectangle: *c_void,
-        FillRectangle: *c_void,
-        DrawRoundedRectangle: *c_void,
-        FillRoundedRectangle: *c_void,
-        DrawEllipse: *c_void,
-        FillEllipse: *c_void,
-        DrawGeometry: *c_void,
-        FillGeometry: *c_void,
-        FillMesh: *c_void,
-        FillOpacityMask: *c_void,
-        DrawBitmap: *c_void,
-        DrawText: *c_void,
-        DrawTextLayout: *c_void,
-        DrawGlyphRun: *c_void,
-        SetTransform: *c_void,
-        GetTransform: *c_void,
-        SetAntialiasMode: *c_void,
-        GetAntialiasMode: *c_void,
-        SetTextAntialiasMode: *c_void,
-        GetTextAntialiasMode: *c_void,
-        SetTextRenderingParams: *c_void,
-        GetTextRenderingParams: *c_void,
-        SetTags: *c_void,
-        GetTags: *c_void,
-        PushLayer: *c_void,
-        PopLayer: *c_void,
-        Flush: *c_void,
-        SaveDrawingState: *c_void,
-        RestoreDrawingState: *c_void,
-        PushAxisAlignedClip: *c_void,
-        PopAxisAlignedClip: *c_void,
-        Clear: *c_void,
-        BeginDraw: *c_void,
-        EndDraw: *c_void,
-        GetPixelFormat: *c_void,
-        SetDpi: *c_void,
-        GetDpi: *c_void,
-        GetSize: *c_void,
-        GetPixelSize: *c_void,
-        GetMaximumBitmapSize: *c_void,
-        IsSupported: *c_void,
-        // ID2D1BitmapRenderTarget
-        GetBitmap: *c_void,
+        // ID2D1Device
+        CreateDeviceContext: *c_void,
+        CreatePrintControl: *c_void,
+        SetMaximumTextureMemory: *c_void,
+        GetMaximumTextureMemory: *c_void,
+        ClearResources: *c_void,
+        // ID2D1Device1
+        GetRenderingPriority: *c_void,
+        SetRenderingPriority: *c_void,
+        CreateDeviceContext1: *c_void,
+        // ID2D1Device2
+        CreateDeviceContext2: *c_void,
+        FlushDeviceContexts: *c_void,
+        GetDxgiDevice: *c_void,
+        // ID2D1Device3
+        CreateDeviceContext3: *c_void,
+        // ID2D1Device4
+        CreateDeviceContext4: *c_void,
+        SetMaximumColorGlyphCacheMemory: *c_void,
+        GetMaximumColorGlyphCacheMemory: *c_void,
+        // ID2D1Device5
+        CreateDeviceContext5: *c_void,
+        // ID2D1Device6
+        CreateDeviceContext6: *c_void,
     },
     usingnamespace os.IUnknown.Methods(Self);
     usingnamespace IResource.Methods(Self);
 };
 
-pub const IHwndRenderTarget = extern struct {
-    const Self = @This();
-    vtbl: *const extern struct {
-        // IUnknown
-        QueryInterface: fn (*Self, *const os.GUID, **c_void) callconv(.Stdcall) HRESULT,
-        AddRef: fn (*Self) callconv(.Stdcall) u32,
-        Release: fn (*Self) callconv(.Stdcall) u32,
-        // ID2D1Resource
-        GetFactory: fn (*Self, **IFactory) callconv(.Stdcall) void,
-        // ID2D1RenderTarget
-        CreateBitmap: *c_void,
-        CreateBitmapFromWicBitmap: *c_void,
-        CreateSharedBitmap: *c_void,
-        CreateBitmapBrush: *c_void,
-        CreateSolidColorBrush: *c_void,
-        CreateGradientStopCollection: *c_void,
-        CreateLinearGradientBrush: *c_void,
-        CreateRadialGradientBrush: *c_void,
-        CreateCompatibleRenderTarget: *c_void,
-        CreateLayer: *c_void,
-        CreateMesh: *c_void,
-        DrawLine: *c_void,
-        DrawRectangle: *c_void,
-        FillRectangle: *c_void,
-        DrawRoundedRectangle: *c_void,
-        FillRoundedRectangle: *c_void,
-        DrawEllipse: *c_void,
-        FillEllipse: *c_void,
-        DrawGeometry: *c_void,
-        FillGeometry: *c_void,
-        FillMesh: *c_void,
-        FillOpacityMask: *c_void,
-        DrawBitmap: *c_void,
-        DrawText: *c_void,
-        DrawTextLayout: *c_void,
-        DrawGlyphRun: *c_void,
-        SetTransform: *c_void,
-        GetTransform: *c_void,
-        SetAntialiasMode: *c_void,
-        GetAntialiasMode: *c_void,
-        SetTextAntialiasMode: *c_void,
-        GetTextAntialiasMode: *c_void,
-        SetTextRenderingParams: *c_void,
-        GetTextRenderingParams: *c_void,
-        SetTags: *c_void,
-        GetTags: *c_void,
-        PushLayer: *c_void,
-        PopLayer: *c_void,
-        Flush: *c_void,
-        SaveDrawingState: *c_void,
-        RestoreDrawingState: *c_void,
-        PushAxisAlignedClip: *c_void,
-        PopAxisAlignedClip: *c_void,
-        Clear: *c_void,
-        BeginDraw: *c_void,
-        EndDraw: *c_void,
-        GetPixelFormat: *c_void,
-        SetDpi: *c_void,
-        GetDpi: *c_void,
-        GetSize: *c_void,
-        GetPixelSize: *c_void,
-        GetMaximumBitmapSize: *c_void,
-        IsSupported: *c_void,
-        // ID2D1HwndRenderTarget
-        CheckWindowState: *c_void,
-        Resize: *c_void,
-        GetHwnd: *c_void,
-    },
-    usingnamespace os.IUnknown.Methods(Self);
-    usingnamespace IResource.Methods(Self);
-};
-
-pub const IDCRenderTarget = extern struct {
-    const Self = @This();
-    vtbl: *const extern struct {
-        // IUnknown
-        QueryInterface: fn (*Self, *const os.GUID, **c_void) callconv(.Stdcall) HRESULT,
-        AddRef: fn (*Self) callconv(.Stdcall) u32,
-        Release: fn (*Self) callconv(.Stdcall) u32,
-        // ID2D1Resource
-        GetFactory: fn (*Self, **IFactory) callconv(.Stdcall) void,
-        // ID2D1RenderTarget
-        CreateBitmap: *c_void,
-        CreateBitmapFromWicBitmap: *c_void,
-        CreateSharedBitmap: *c_void,
-        CreateBitmapBrush: *c_void,
-        CreateSolidColorBrush: *c_void,
-        CreateGradientStopCollection: *c_void,
-        CreateLinearGradientBrush: *c_void,
-        CreateRadialGradientBrush: *c_void,
-        CreateCompatibleRenderTarget: *c_void,
-        CreateLayer: *c_void,
-        CreateMesh: *c_void,
-        DrawLine: *c_void,
-        DrawRectangle: *c_void,
-        FillRectangle: *c_void,
-        DrawRoundedRectangle: *c_void,
-        FillRoundedRectangle: *c_void,
-        DrawEllipse: *c_void,
-        FillEllipse: *c_void,
-        DrawGeometry: *c_void,
-        FillGeometry: *c_void,
-        FillMesh: *c_void,
-        FillOpacityMask: *c_void,
-        DrawBitmap: *c_void,
-        DrawText: *c_void,
-        DrawTextLayout: *c_void,
-        DrawGlyphRun: *c_void,
-        SetTransform: *c_void,
-        GetTransform: *c_void,
-        SetAntialiasMode: *c_void,
-        GetAntialiasMode: *c_void,
-        SetTextAntialiasMode: *c_void,
-        GetTextAntialiasMode: *c_void,
-        SetTextRenderingParams: *c_void,
-        GetTextRenderingParams: *c_void,
-        SetTags: *c_void,
-        GetTags: *c_void,
-        PushLayer: *c_void,
-        PopLayer: *c_void,
-        Flush: *c_void,
-        SaveDrawingState: *c_void,
-        RestoreDrawingState: *c_void,
-        PushAxisAlignedClip: *c_void,
-        PopAxisAlignedClip: *c_void,
-        Clear: *c_void,
-        BeginDraw: *c_void,
-        EndDraw: *c_void,
-        GetPixelFormat: *c_void,
-        SetDpi: *c_void,
-        GetDpi: *c_void,
-        GetSize: *c_void,
-        GetPixelSize: *c_void,
-        GetMaximumBitmapSize: *c_void,
-        IsSupported: *c_void,
-        // ID2D1DCRenderTarget
-        BindDC: *c_void,
-    },
-    usingnamespace os.IUnknown.Methods(Self);
-    usingnamespace IResource.Methods(Self);
-};
-
-pub const IGdiInteropRenderTarget = extern struct {
-    const Self = @This();
-    vtbl: *const extern struct {
-        // IUnknown
-        QueryInterface: fn (*Self, *const os.GUID, **c_void) callconv(.Stdcall) HRESULT,
-        AddRef: fn (*Self) callconv(.Stdcall) u32,
-        Release: fn (*Self) callconv(.Stdcall) u32,
-        // ID2D1GdiInteropRenderTarget
-        GetDC: *c_void,
-        ReleaseDC: *c_void,
-    },
-    usingnamespace os.IUnknown.Methods(Self);
-};
-
-pub const IFactory = extern struct {
+pub const IFactory7 = extern struct {
     const Self = @This();
     vtbl: *const extern struct {
         // IUnknown
@@ -894,23 +776,45 @@ pub const IFactory = extern struct {
         AddRef: fn (*Self) callconv(.Stdcall) u32,
         Release: fn (*Self) callconv(.Stdcall) u32,
         // ID2D1Factory
-        ReloadSystemMetrics: fn (*Self) callconv(.Stdcall) HRESULT,
-        GetDesktopDpi: fn (*Self, *f32, *f32) callconv(.Stdcall) void,
-        // TODO: Add all members.
+        ReloadSystemMetrics: *c_void,
+        GetDesktopDpi: *c_void,
+        CreateRectangleGeometry: *c_void,
+        CreateRoundedRectangleGeometry: *c_void,
+        CreateEllipseleGeometry: *c_void,
+        CreateGeometryGroup: *c_void,
+        CreateTransformedGeometry: *c_void,
+        CreatePathGeometry: *c_void,
+        CreateStrokeStyle: *c_void,
+        CreateDrawingStateBlock: *c_void,
+        CreateWicBitmapRenderTarget: *c_void,
+        CreateHwndRenderTarget: *c_void,
+        CreateDxgiSurfaceRenderTarget: *c_void,
+        CreateDCRenderTarget: *c_void,
+        // ID2D1Factory1
+        CreateDevice: *c_void,
+        CreateStrokeStyle1: *c_void,
+        CreatePathGeometry1: *c_void,
+        CreateDrawingStateBlock1: *c_void,
+        CreateGdiMetafile: *c_void,
+        RegisterEffectFromStream: *c_void,
+        RegisterEffectFromString: *c_void,
+        UnregisterEffect: *c_void,
+        GetRegisteredEffects: *c_void,
+        GetEffectProperties: *c_void,
+        // ID2D1Factory2
+        CreateDevice1: *c_void,
+        // ID2D1Factory3
+        CreateDevice2: *c_void,
+        // ID2D1Factory4
+        CreateDevice3: *c_void,
+        // ID2D1Factory5
+        CreateDevice4: *c_void,
+        // ID2D1Factory6
+        CreateDevice5: *c_void,
+        // ID2D1Factory7
+        CreateDevice6: *c_void,
     },
     usingnamespace os.IUnknown.Methods(Self);
-    usingnamespace IFactory.Methods(Self);
-
-    fn Methods(comptime T: type) type {
-        return extern struct {
-            pub inline fn ReloadSystemMetrics(self: *T) HRESULT {
-                return self.vtbl.ReloadSystemMetrics(self);
-            }
-            pub inline fn GetDesktopDpi(self: *T, dpi_x: *f32, dpi_y: *f32) usize {
-                self.vtbl.GetDesktopDpi(self, dpi_x, dpi_y);
-            }
-        };
-    }
 };
 
 pub const IID_IFactory = os.GUID{
